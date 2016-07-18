cdbaabac3ff44585f11afaf92814068ab095a44e
Michael Dippery <michael@monkey-robot.com>
2016-07-17 19:02:00 -0700

    Create skeleton Xcode project

---
+++ Animals/Animals/AnimalViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AnimalViewController.swift
+//  Animals
+//
+//  Created by Michael Dippery on 7/17/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class AnimalViewController: UIViewController {
+}
